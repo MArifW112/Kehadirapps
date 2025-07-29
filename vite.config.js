4aadccc8a9234ce726748610f8c01267a630bb0d
fzraaa <fauziahazzahra241@gmail.com>
2025-07-29 15:41:17 +0700

    Fix: output manifest.json to public/build

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,16 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite'
+import laravel from 'laravel-vite-plugin'
 
 export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
+            buildDirectory: 'build', // penting!
         }),
     ],
     build: {
         manifest: true,
-        outDir: 'public/build',
-        rollupOptions: {
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-        },
+        outDir: 'public/build', // penting!
     },
-});
+})
