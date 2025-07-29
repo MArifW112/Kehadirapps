22f133ef8b58217ee6a8e84f9b3217542396e5ee
fzraaa <fauziahazzahra241@gmail.com>
2025-07-29 20:17:47 +0700

    Bug Fix Logout Last

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,7 +6,6 @@ use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Session\TokenMismatchException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
-use Throwable;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
