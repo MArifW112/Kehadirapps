3f3de689cc45f37845a462926506705867e79e42
fzraaa <fauziahazzahra241@gmail.com>
2025-07-29 15:49:42 +0700

    Ayolaaah beneer

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,23 @@
-import { defineConfig } from 'vite'
-import laravel from 'laravel-vite-plugin'
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
-            buildDirectory: 'build', // penting!
+            buildDirectory: 'build', // <-- ini penting!
         }),
     ],
     build: {
+        outDir: 'public/build', // pastikan outputnya di sini
         manifest: true,
-        outDir: 'public/build', // penting!
+        rollupOptions: {
+            input: {
+                app: 'resources/js/app.js',
+                style: 'resources/css/app.css', // kalau mau CSS juga
+            },
+        },
+        emptyOutDir: true,
     },
-})
+});
